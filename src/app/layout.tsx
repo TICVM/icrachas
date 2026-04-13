77b972f2e8c2444e95d958304b69fcf9cb1e719b
Herbert Silva Pacheco <cvmspinfo@gmail.com>
2026-04-13 08:30:53 -0300

    Add files via upload

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type {Metadata} from 'next';
+import './globals.css';
+import { Toaster } from "@/components/ui/toaster"
+import { FirebaseClientProvider } from '@/firebase/client-provider';
+
+export const metadata: Metadata = {
+  title: 'Identifica mais - Seu crachá em um clique!',
+  description: 'Sistema avançado para criação e geração de crachás estudantis personalizados.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+      </head>
+      <body className="font-body antialiased">
+        <FirebaseClientProvider>
+          {children}
+          <Toaster />
+        </FirebaseClientProvider>
+      </body>
+    </html>
+  );
+}

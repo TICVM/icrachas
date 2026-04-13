1c21a675b3fb5d00b619b9a00b846c192a18303b
Herbert Silva Pacheco <cvmspinfo@gmail.com>
2026-04-13 09:54:57 -0300

    Add files via upload

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7359227371-d395b",
+  "appId": "1:606317699870:web:496739b818c733c90edf0f",
+  "apiKey": "AIzaSyCnbFSL9u-E6x7I4dlja2BORY-Fm1Eu-Gg",
+  "authDomain": "studio-7359227371-d395b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "606317699870"
+};
